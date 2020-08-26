672d2677e8738e48323492588b6e944a3ba3f713
Pete Glassey <peter.glassey@education.govt.nz>
2020-08-26 21:16:34 +1200

    disable ap0
    - disable ap0; give wlan0 static IP

--- etc/start-access-point.sh
+++ etc/start-access-point.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-#apt -y install dnsmasq dhcpcd hostapd
+apt -y install dnsmasq dhcpcd hostapd
 
 parent_device="wlan0"
 ssid="PICAM"
-pass="Olav0101"
+pass="picam4tw"
 ipaddress="192.168.16.1/24"
 dhcprange="192.168.16.50,192.168.16.200,255.255.255.0,24h"
 
@@ -34,13 +34,13 @@ done
 #- determine country and channel
 #- Raspberry3 phys interface has one channel only
 #------------------------------------------------------------------------
-country=$(awk -F '=' '/^country/ {print $2}' /etc/wpa_supplicant/wpa_supplicant.conf)
-channel=$(iwlist $parent_device channel | awk '/Current Frequency/ { sub(/\)$/,"",$NF); print $NF }')
+#country='NZ' #$(awk -F '=' '/^country/ {print $2}' /etc/wpa_supplicant/wpa_supplicant.conf)
+#channel=$(iwlist $parent_device channel | awk '/Current Frequency/ { sub(/\)$/,"",$NF); print $NF }')
 
 # is country a valid 2 character code?
-[[ "$country" =~ ^[A-Z][A-Z]$ ]] || country='US'
+#[[ "$country" =~ ^[A-Z][A-Z]$ ]] || country='US'
 # is channel a valid integer?
-[[ "$channel" =~ ^[0-9]+$ ]] || channel=7
+#[[ "$channel" =~ ^[0-9]+$ ]] || channel=7
 
 #echo "channel: $channel"
 #echo "country: $country"
@@ -50,6 +50,17 @@ channel=$(iwlist $parent_device channel | awk '/Current Frequency/ { sub(/\)$/,"
 #------------------------------------------------------------------------
 #grep -q 'denyinterfaces ap0' /etc/dhcpcd.conf || echo 'denyinterfaces ap0' >> /etc/dhcpcd.conf
 
+# Configure wlan0
+grep -q "auto wlan0" /etc/network/interfaces 2>/dev/null
+if [[ $? -ne 0 ]]
+then
+  cat >> /etc/network/interfaces <<EOF
+auto wlan0
+iface wlan0 inet static
+  address 192.168.16.1
+  netmask 255.255.255.0
+EOF
+fi
 #------------------------------------------------------------------------
 #- Populate hostapd.conf
 #------------------------------------------------------------------------
@@ -57,11 +68,12 @@ grep -q "ssid=${ssid}"  /etc/hostapd/hostapd.conf 2>/dev/null
 if [[ $? -ne 0 ]]
 then
  cat > /etc/hostapd/hostapd.conf <<EOF
-interface=ap0
+interface=wlan0
 driver=nl80211
 ssid=${ssid}
+ieee80211n=1
 hw_mode=g
-channel=7
+channel=6
 wmm_enabled=0
 macaddr_acl=0
 auth_algs=1
@@ -87,24 +99,23 @@ fi
 #------------------------------------------------------------------------
 #- Populate `/etc/dnsmasq.conf`
 #------------------------------------------------------------------------
-grep -q 'interface=ap0' /etc/dnsmasq.conf
+grep -q 'interface=wlan0' /etc/dnsmasq.conf
 if [[ $? -ne 0 ]]
 then
   cat >> /etc/dnsmasq.conf << EOF
 
 # wlan access point
-except-interface=wlan0
-interface=ap0
+interface=wlan0
 dhcp-range=${dhcprange}
 EOF
 fi
 
 #------------------------------------------------------------------------
-# setup ap0
+# setup wlan0
 # iwlist scan
 # iw list
 #------------------------------------------------------------------------
-ip a s dev ap0 > /dev/null 2>&1
+ip a s dev wlan0 > /dev/null 2>&1
 if [[ $? -ne 0 ]]
 then
   echo "Setting up AP: $ssid"
@@ -115,12 +126,14 @@ then
   #ip link set ${parent_device} down
   #iw dev ${parent_device} del
   #iw phy phy0 interface add ${parent_device} type station
-  iw phy phy0 interface add ap0 type __ap
-  ip link set ap0 address $macaddr
-  ip address add ${ipaddress} dev ap0
+  #iw phy phy0 interface add ap0 type __ap
+  #ip link set ap0 address $macaddr
+  #ip address add ${ipaddress} dev ap0
   #ip link set ${parent_device} up
-  ip link set ap0 up
+  #ip link set ap0 up
   systemctl start hostapd
   systemctl restart dnsmasq
   systemctl restart dhcpcd
+  systemctl enable dnsmasq
+  systemctl enable hostapd
 fi
